4a2da60ffc585d4c6b6149658f37bff2f11fec45
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2026-02-08 20:24:27 +0600

    Slider carousel added to Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,43 @@ import GenreSection from "../components/GenreSection";
 import MovieCardSkeleton from "@/components/MovieCardSkeleton";
 import Hero from "@/components/Hero";
 import HeroSkeleton from "@/components/HeroSkeleton";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function HomePage() {
   const { genres, topRated, trending, isLoading, isTrendingLoading } =
     useHomeData();
-  const heroMovie = trending ? trending[0] : null;
+
+  const settings = {
+    arrows: false,
+    fade: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    waitForAnimate: true,
+    pauseOnHover: false,
+  };
 
   return (
     <main>
+      {/* Hero section */}
       {isTrendingLoading ? (
         <HeroSkeleton />
-      ) : heroMovie ? (
-        <Hero movie={heroMovie} />
+      ) : trending?.length ? (
+        <div className="slider-container">
+          <Slider {...settings}>
+            {trending.slice(0, 5).map((item) => (
+              <Hero key={item.id} movie={item} />
+            ))}
+          </Slider>
+        </div>
       ) : null}
+
+      {/* Top rated movies */}
       <section className="container mx-auto px-4 py-8">
         <h2 className="text-lg md:text-2xl font-bold mb-6">Top Rated Movies</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
@@ -29,6 +53,7 @@ export default function HomePage() {
         </div>
       </section>
 
+      {/* Movies by Genre */}
       <section className="container mx-auto px-4 py-8 space-y-8">
         <h2 className="text-lg md:text-2xl font-bold">Movies by Genre</h2>
         {genres?.length
